7fe3b29805fee5c0f02f8b3913592e1a6af35207
aleksCC <al3kchr3@gmail.com>
2019-03-17 13:29:59 +0100

    wątki

--- PSR/Program.cs
+++ PSR/Program.cs
@@ -5,19 +5,42 @@ namespace PSR
 {
     class Program
     {
-        static void Main()
-        {            
+        static void Main(string[] args)
+        {
+            int numberOfThreads = 1;
+            /*
+             * Parametr wejściowy można zmienić w nazwa_projektu->Properties->Debug->Command Line
+             * */
+            if(args.Length>0)
+            {
+                if(!int.TryParse(args[0],out numberOfThreads))
+                {
+                    Console.WriteLine("Błąd argumentu liczby wątków! Wybrano wartość domyślną.");
+                }
+            }
+
+            Console.WriteLine("Liczba wątków do uruchomienia algorytmu: " + numberOfThreads);
+
+            ConcurrentProgram concurrent = new ConcurrentProgram(numberOfThreads);
+            concurrent.Start();
+
+            /*
             Graph graph = new Graph();
-            DijkstraAlgorithm dijkstra = new DijkstraAlgorithm();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
 
             for (int i = 0; i < graph.matrix.GetLength(0); i++)
             {
-                int[] suma = dijkstra.Dijkstra(graph.matrix, i, graph.matrix.GetLength(0));
+                int[] suma = DijkstraAlgorithm.Dijkstra(graph.matrix, i, graph.matrix.GetLength(0));
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Łączna długość najkrótszych ścieżek: " + suma.Sum());
                 Console.ResetColor();
             }
-            
+
+            watch.Stop();
+            var elapsedMiliseconds = watch.ElapsedMilliseconds;
+            Console.WriteLine("Total time:"+elapsedMiliseconds+"ms");
+            */
             Console.ReadKey();
         }
     }
