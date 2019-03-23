c7c7695d2271cf53723b78cec70b3c25dca31b98
Daniel <d.brzeznicki@wp.pl>
2019-03-23 09:51:37 +0100

    Wczytywanie pliku

--- PSR/Program.cs
+++ PSR/Program.cs
@@ -6,7 +6,14 @@ namespace PSR
     class Program
     {
         static void Main()
-        {            
+        {
+
+            //proba odczytu
+            ReadFile read = new ReadFile();
+            int [][] matrix = read.readData(@"C:\Users\Daniel\Desktop\macierz.txt");
+
+            Console.WriteLine(matrix[1][1]);
+
             Graph graph = new Graph();
             DijkstraAlgorithm dijkstra = new DijkstraAlgorithm();
 
@@ -17,7 +24,7 @@ namespace PSR
                 Console.WriteLine("Łączna długość najkrótszych ścieżek: " + suma.Sum());
                 Console.ResetColor();
             }
-            
+
             Console.ReadKey();
         }
     }
--- PSR/ReadFile.cs
+++ PSR/ReadFile.cs
@@ -1,7 +1,40 @@
-﻿namespace PSR
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace PSR
 {
     public class ReadFile
     {
         //funkcja odczytujaca dane z pliku i tworząca obiekt grafu z tymi danymi
+        public int[][] readData(string path)
+        {
+            int[][] matrix = File.ReadAllLines(path)
+                   .Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray())
+                   .ToArray();
+
+            String report = String.Join(Environment.NewLine, matrix
+                .Select(line => String.Join(" ", line)));
+
+            Console.Write(report);
+
+            return matrix;
+        }
+
+        public T[,] JaggedToMultidimensional<T>(T[][] jaggedArray)
+        {
+            int rows = jaggedArray.Length;
+            int cols = jaggedArray.Max(subArray => subArray.Length);
+            T[,] array = new T[rows, cols];
+            for (int i = 0; i < rows; i++)
+            { 
+                cols = jaggedArray[i].Length;
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = jaggedArray[i][j];
+                }
+            }
+            return array;
+        }
     }
 }
